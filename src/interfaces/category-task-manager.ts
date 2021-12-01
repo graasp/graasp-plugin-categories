ebab4fae08329c57557be536caac5c732ee0d2e1
Louise Wang <louise.w1997@gmail.com>
2021-12-01 19:49:13 +0100

    fix: rename columns

--- src/interfaces/category-task-manager.ts
+++ src/interfaces/category-task-manager.ts
@@ -2,10 +2,12 @@
 import { Actor, Task } from 'graasp';
 // local
 import { Category } from './category';
+import { ItemCategory } from './item-category';
 
 export interface CategoryTaskManager<A extends Actor = Actor> {
   getGetCategoryTaskName(): string;
 
   createGetCategoryTask(actor: A, objectId: string): Task<A, Category>;
   createGetCategoriesByTypeTask(actor: A, type: string[]): Task<A, Category[]>;
+  createGetItemCategoryTask(actor: A, itemId: string): Task<A, ItemCategory[]>;
 }
