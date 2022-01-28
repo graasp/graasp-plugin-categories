0ee426bf48822d18f7df5dfee62cebe183f1798b
Louise Wang <louise.w1997@gmail.com>
2022-01-28 13:05:26 +0100

    fix: add concatenatedIds to schema

--- src/tasks/get-items-by-category-task.ts
+++ src/tasks/get-items-by-category-task.ts
@@ -37,8 +37,9 @@ export class getItemsByCategoriesTask extends BaseCategoryTask<Item[]> {
       );
       return itemIds;
     }));
-
-    const items = itemIdsList.length > 1 ? itemIdsList.reduce((a, b) => a.filter((c) => getItemIdsArray(b).includes(c.id))) : itemIdsList[0];
+    
+    // get the intersection of itemIdsList
+    const items = itemIdsList.reduce((a, b) => a.filter((c) => getItemIdsArray(b).includes(c.id)));
     this.status = 'OK';
     this._result = items;
   }
