8ad1483c0ac3cd04665f0bfa57e4b5195cf1e249
pyphilia <pyphilia@gmail.com>
2022-08-12 12:21:31 +0200

    refactor: export admin plugin

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default } from './service-api';
 export { default as publicPlugin } from './publicPlugin';
+export { default as adminPlugin } from './adminPlugin';
 export { CategoryService } from './db-service';
 export { ItemCategory } from './interfaces/item-category';
 export { Category } from './interfaces/category';
